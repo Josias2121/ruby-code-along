dcde73ab5820396c3b508b0cb7b9c1012c106025
josias2121 <josias.vidrio.simontech.2016@gmail.com>
2016-03-08 23:19:47 +0000

    almost finished

--- app.rb
+++ app.rb
@@ -3,8 +3,17 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
+    erb :index
+  end
+  
+  post '/' do
+    @user = { 
+      profilepic: "#{params["propic"]}",
+      username: "#{params["username"]}",
+      email: "#{params["email"]}",
+      password: "#{params["password"]}",
+      }
   erb :dashboard
   end
-   
-  
+    
 end
